be92b56875d3191c9803f63263e17da09f92c27b
divyaasonara <divya.sonara@datahash.com>
2024-11-11 17:42:45 +0530

    style changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-417f1a35'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.04bcd018jd8"
+    "revision": "0.prehhpe79r8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
